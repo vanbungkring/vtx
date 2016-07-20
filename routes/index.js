7d7a24ab506e099a5932a742d480de26fca74959
vanbungkring <prasetyo.mailbox@gmail.com>
2016-07-20 14:16:13 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,27 @@ var request = require('request');
 
 
 /* GET home page. */
+router.post('/', function(req, res, next) {
+    var transaction_details = {};
+    transaction_details.order_id = 'order-' + makeid();
+    transaction_details.gross_amount = '1000'
+    var options = {
+        url: 'https://app.sandbox.veritrans.co.id/snap/v1/pay/',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic VlQtc2VydmVyLUNoWTBwZmJ2VUNGaXV5WjhNZjB0S0o0ag=='
+        },
+        body: JSON.stringify({
+            'transaction_details': transaction_details
+        }),
+        method: 'POST'
+    };
+    request(options, function(error, response, body) {
+        if (!error) {
+            res.json(JSON.parse(body));
+        }
+    })
+});
 router.get('/', function(req, res, next) {
     var transaction_details = {};
     transaction_details.order_id = 'order-' + makeid();
@@ -25,7 +46,6 @@ router.get('/', function(req, res, next) {
         }
     })
 });
-
 function makeid() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
